948c23007a30cafd77f0ad70bf75566c36f21ab8
CoderNuby <correofake12a@gmail.com>
2020-11-12 12:46:42 -0700

    rutas working

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+import { dashboardRoutes } from './dashboard/dashboard.routes';
+
+const routes: Routes = [
+  { 
+    path: '',
+    component: DashboardComponent,
+    children: dashboardRoutes
+  },
+  { path: '**', redirectTo: ''},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
